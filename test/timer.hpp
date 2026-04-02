20d9f9a1f9732f9f9e5137d6ad0ea0f10c6f329f
sihengt <siheng.teng@gmail.com>
2026-04-02 07:09:35 +0000

    formatted to my preference

--- test/timer.hpp
+++ test/timer.hpp
@@ -1,24 +1,26 @@
 #include <chrono>
 
-namespace JPS {
-  class Timer {
+namespace JPS
+{
+class Timer
+{
     typedef std::chrono::high_resolution_clock high_resolution_clock;
     typedef std::chrono::milliseconds milliseconds;
-    public:
+
+public:
     explicit Timer(bool run = false)
     {
-      if (run)
-        Reset();
+        if (run)
+            Reset();
     }
-    void Reset()
-    {
-      _start = high_resolution_clock::now();
-    }
+    void Reset() { _start = high_resolution_clock::now(); }
     milliseconds Elapsed() const
     {
-      return std::chrono::duration_cast<milliseconds>(high_resolution_clock::now() - _start);
+        return std::chrono::duration_cast<milliseconds>(
+            high_resolution_clock::now() - _start);
     }
-    private:
+
+private:
     high_resolution_clock::time_point _start;
-  };
-}
+};
+} // namespace JPS
